881b09a568c0bd06b2594d01c6a01604ab159d8e
grigmily <grigmily@gmail.com>
2022-06-28 00:10:19 +0300

    feat: counter and toggleProperty

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,8 +16,6 @@ export default class App extends Component {
       this.createTodoItem("Make Awesome App"),
       this.createTodoItem("Have a lunch")
     ],
-    toDo: 1,
-    done: 2
   };
 
   createTodoItem(label) {
@@ -27,7 +25,7 @@ export default class App extends Component {
       done: false,
       id: this.minId++
     }
-  }
+  };
 
   deleteItem = (id) => {
     this.setState(({todoData}) => {
@@ -59,25 +57,45 @@ export default class App extends Component {
 
   };
 
-
+  toggleProperty(arr, id, propName) {
+    const idx = arr.findIndex((el) => el.id === id);
+    const oldItem = arr[idx];
+    const newItem = {...oldItem,
+      [propName]: !oldItem[propName]};
+    return [
+      ...arr.slice(0, idx),
+      newItem,
+      ...arr.slice(idx+1)
+    ];
+  };
 
   onToggleDone = (id) => {
     this.setState(({ todoData }) => {
-      
+      return{
+        todoData: this.toggleProperty(todoData, id, 'done')
+      };
     });
   };
 
   onToggleImportant = (id) => {
-    console.log('Toggle Important: ', id);
+    this.setState(({ todoData }) => {
+      return{
+        todoData: this.toggleProperty(todoData, id, 'important')
+      };
+    });
   };
 
   render() {
+    const { todoData } = this.state;
+    const doneCount = todoData.filter((el) => el.done).length;
+    const todoCount = todoData.length - doneCount;
+
     return (
       <div className="app-position">
-        <AppHeader toDo={this.state.toDo} done={this.state.done}/>
+        <AppHeader toDo={todoCount} done={doneCount}/>
         <SearchPanel/>
         <TodoList
-          todos={this.state.todoData}
+          todos={todoData}
           onDeleted={(id) => this.deleteItem(id)}
           onToggleImportant={this.onToggleImportant}
           onToggleDone={this.onToggleDone}
